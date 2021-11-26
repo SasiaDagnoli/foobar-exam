7c6c85335aac429a1bda515a84ebb2d7e68545a5
Sasia Dagnoli <sasi0026@stud.kea.dk>
2021-11-26 21:11:39 +0100

    all data fetched

--- main.js
+++ main.js
@@ -1,6 +1,71 @@
-import './style.css'
+//import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+"use strict";
+
+window.addEventListener("DOMContentLoaded", hentData);
+
+async function hentData() {
+  const resultat = await fetch("https://bandoen.herokuapp.com/");
+  const data = await resultat.json();
+
+  console.log("data", data);
+  visData(data);
+}
+
+function visData(data) {
+  const medarbejderinfo = document.querySelector(".medarbejderinfo");
+  const temp = document.querySelector(".medarbejdere");
+
+  data.bartenders.forEach((elm) => {
+    const klon = temp.cloneNode(true).content;
+    klon.querySelector(".medarbejder").textContent = elm.name;
+    klon.querySelector(".status").textContent = elm.status;
+
+    medarbejderinfo.appendChild(klon);
+  });
+
+  document.querySelector(".inline").textContent = `KØ: ${data.queue.length}`;
+
+  const betjenes = document.querySelector(".hvembetjenes");
+  const tempbetjenes = document.querySelector(".betjenes");
+
+  data.serving.forEach((elm) => {
+    const klon = tempbetjenes.cloneNode(true).content;
+    klon.querySelector(".betjenesnu").textContent = `${elm.id}`;
+
+    betjenes.appendChild(klon);
+  });
+
+  const dagens = document.querySelector(".dagens");
+  const tempdagens = document.querySelector(".tempdagens");
+
+  data.taps.forEach((elm) => {
+    const klon = tempdagens.cloneNode(true).content;
+    klon.querySelector(".navn").textContent = elm.beer;
+
+    dagens.appendChild(klon);
+  });
+
+  const lager = document.querySelector(".lager");
+  const templager = document.querySelector(".templager");
+
+  data.storage.forEach((elm) => {
+    const klon = templager.cloneNode(true).content;
+    klon.querySelector(".navn").textContent = elm.name;
+
+    lager.appendChild(klon);
+  });
+  /* const queue = document.querySelector(".queue");
+  const tempqueue = document.querySelector(".thequeue");
+
+  data.queue.forEach((elm) => {
+    const klon = tempqueue.cloneNode(true).content;
+    klon.querySelector(".inqueue").textContent = `Kø: ${queue.length}`;
+
+    queue.appendChild(klon);
+  }); */
+}
+/* setInterval(function () {
+  hentData();
+}, 10000);
+ */

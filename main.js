22880312d5fbcacb3609c154c19fa87fbb1883da
Sasia Dagnoli <sasi0026@stud.kea.dk>
2021-12-07 14:15:27 +0100

    updater function for staff

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ async function hentData(first = true) {
   if (first) {
     hentTyper();
     visData(data);
+    showDataCstm(data);
   } else {
     opdaterData(data);
+    updateCstmData(data);
   }
 }
 
@@ -40,39 +42,58 @@ function visTypeData(dataTyper) {
 
 function opdaterData(data) {
   document.querySelector(".inline").textContent = `KØ: ${data.queue.length}`;
+
   data.bartenders.forEach((elm) => {
     document.querySelector(
       `[data-bartendername="${elm.name}"] .status`
     ).textContent = elm.status;
   });
 
+  const betjenes = document.querySelector(".hvembetjenes-data");
+  const tempbetjenes = document.querySelector(".betjenes");
+  betjenes.innerHTML = "";
+
   data.serving.forEach((elm) => {
-    document.querySelector(".betjenesnu").textContent = `${elm.id}`;
+    const klon = tempbetjenes.cloneNode(true).content;
+    klon.querySelector(".betjenesnu").textContent = elm.id;
+
+    betjenes.appendChild(klon);
   });
 
   data.taps.forEach((elm) => {
-    document.querySelector(".dagensdiv").dataset.name = elm.beer;
-    document.querySelector(".navn").textContent = elm.beer;
+    const elmBeer = document.querySelector(
+      `.dagensdiv[data-name="${elm.beer}"]`
+    );
+    console.log("Beer", elmBeer, elm.beer);
+    elmBeer.querySelector(".navn").textContent = elm.beer;
   });
 
   data.storage.forEach((elm) => {
-    document.querySelector(
-      `[data-storage-bar="${elm.amount}"] .antal`
-    ).style.width = elm.amount.toString() + "0px";
-    document.querySelector(".navn").textContent = `${elm.name} ${elm.amount}`;
+    const elmAmount = document.querySelector(
+      `.lagerflex[data-storagebar="${elm.name}"]`
+    );
+    //console.log("AMOUNTBEER", elmAmount);
+    elmAmount.querySelector(".navn").textContent = elm.name;
+    elmAmount.querySelector(".antaltal").textContent = elm.amount;
+
+    // console.log("AMOUNT", elm.amount, elm.name);
+    elmAmount
+      .querySelector(".antal")
+      .style.setProperty("width", elm.amount.toString() + "0px");
     if (elm.amount > 5) {
-      document.querySelector(".antal").classList.add("colorover5");
-      document.querySelector(".antal").classList.remove("colorunder5");
+      document
+        .querySelector(".antal")
+        .classList.replace(".colorunder5", "colorover5");
     } else {
-      console.log("RØD");
-      document.querySelector(".antal").classList.add("colorunder5");
-      document.querySelector(".antal").classList.remove("colorover5");
+      document
+        .querySelector(".antal")
+        .classList.replace(".colorover5", "colorunder5");
     }
   });
 
   const queueSize = [];
+  console.log("KØEM", queueSize);
   if (document.querySelectorAll(".baren").length < 18) {
-    console.log("JA", document.querySelectorAll(".baren").length);
     queueSize.push(data.queue.length);
   } else {
     let elements = document.getElementsByClassName("baren");
@@ -95,6 +116,10 @@ function opdaterData(data) {
   });
 }
 
+/* import { taps } from "./taps.js";
+
+taps(); */
+
 function visData(data) {
   const medarbejderinfo = document.querySelector(".medarbejderinfo-data");
   const temp = document.querySelector(".medarbejdere");
@@ -113,7 +138,7 @@ function visData(data) {
 
   data.serving.forEach((elm) => {
     const klon = tempbetjenes.cloneNode(true).content;
-    klon.querySelector(".betjenesnu").textContent = `${elm.id}`;
+    klon.querySelector(".betjenesnu").textContent = elm.id;
 
     betjenes.appendChild(klon);
   });
@@ -121,11 +146,20 @@ function visData(data) {
   const dagens = document.querySelector(".dagens-data");
   const tempdagens = document.querySelector(".tempdagens");
 
+  let imgName;
+  data.taps.forEach((elm) => {
+    const nameLower = elm.beer.toLowerCase();
+    const lowerOneWord = nameLower.replaceAll(" ", "");
+    imgName = `${lowerOneWord}.png`;
+    console.log("LOWERCASE", imgName);
+  });
+
   data.taps.forEach((elm) => {
     const klon = tempdagens.cloneNode(true).content;
     klon.querySelector(".dagensdiv").dataset.name = elm.beer;
     klon.querySelector(".navn").textContent = elm.beer;
-
+    klon.querySelector(".ontapimg").src = `img/${imgName}`;
+    console.log("HEJ", imgName);
     dagens.appendChild(klon);
   });
 
@@ -134,13 +168,13 @@ function visData(data) {
 
   data.storage.forEach((elm) => {
     const klon = templager.cloneNode(true).content;
-    klon.querySelector(".lagerflex").dataset.storageBar = elm.amount;
-    klon.querySelector(".navn").textContent = `${elm.name} ${elm.amount}`;
+    klon.querySelector(".lagerflex").dataset.storagebar = elm.name;
+    klon.querySelector(".navn").textContent = elm.name;
+    klon.querySelector(".antaltal").textContent = elm.amount;
     klon.querySelector(".antal").style.width = elm.amount.toString() + "0px";
     if (elm.amount > 5) {
       klon.querySelector(".antal").classList.add("colorover5");
     } else {
-      console.log("RØD");
       klon.querySelector(".antal").classList.add("colorunder5");
     }
     lager.appendChild(klon);
@@ -170,10 +204,55 @@ function visData(data) {
     bar.appendChild(klon);
   });
 }
-/* hentTyper(); */
+
 setInterval(function () {
   hentData(false);
 }, 10000);
+
+function updateCstmData(data) {
+  document.querySelector(
+    ".inlinecstm"
+  ).textContent = `QUEUE: ${data.queue.length}`;
+}
+
+function showDataCstm(data) {
+  document.querySelector(
+    ".inlinecstm"
+  ).textContent = `QUEUE: ${data.queue.length}`;
+
+  const todays = document.querySelector(".datatodaycstm");
+  const temptoday = document.querySelector(".tempdagens");
+
+  data.taps.forEach((elm) => {
+    const clone = temptoday.cloneNode(true).content;
+    clone.querySelector(".dagensdiv").dataset.name = elm.beer;
+    clone.querySelector(".navn").textContent = elm.beer;
+
+    todays.appendChild(clone);
+  });
+
+  const serving = document.querySelector(".servingnowcstm");
+  const tempserving = document.querySelector(".betjenes");
+
+  data.serving.forEach((elm) => {
+    const clone = tempserving.cloneNode(true).content;
+    clone.querySelector(".betjenesnu").textContent = elm.id;
+
+    serving.appendChild(clone);
+  });
+}
+
+/* function getLabels(data) {
+  let imgName;
+  data.taps.forEach((elm) => {
+    const nameLower = elm.beer.toLowerCase();
+    const lowerOneWord = nameLower.replaceAll(" ", "");
+    imgName = `${lowerOneWord}.png`;
+    console.log("LOWERCASE", imgName);
+  });
+
+  return imgName;
+} */
 
 countdown();
 
@@ -199,8 +278,27 @@ function countdown() {
     document.getElementById("hours").innerHTML = hh;
     document.getElementById("minutes").innerHTML = mm;
     document.getElementById("seconds").innerHTML = ss;
+    document.getElementById("hourscstm").innerHTML = hh;
+    document.getElementById("minutescstm").innerHTML = mm;
+    document.getElementById("secondscstm").innerHTML = ss;
     setTimeout(tick, 1000);
   }
+
+  const loginForm = document.getElementById("login-form");
+  const loginButton = document.getElementById("login-form-submit");
+
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "manager" && password === "manager") {
+      document.querySelector(".forcustomers").classList.add("hidden");
+      document.querySelector(".forstaff").classList.remove("hidden");
+    } else {
+      alert("Login information not correct");
+    }
+  });
 
   document.addEventListener("DOMContentLoaded", tick);
 }
